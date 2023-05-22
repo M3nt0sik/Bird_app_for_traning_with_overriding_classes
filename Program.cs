9a192ce4cd95ca5153ee4475206ba4c2201fa70a
Kamil <mentos.96@o2.pl>
2023-05-22 19:59:39 +0200

    Write main function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Bird bird;
+        while (true)
+        {
+            Console.Write("\nWciśnij G - golab , S - strus : ");
+            char key = char.ToUpper(Console.ReadKey().KeyChar);
+            switch (key)
+            {
+                case 'G':
+                    bird = new Pigeon();
+                    break;
+                case 'S':
+                    bird = new Ostrich();
+                    break;
+                default:
+                    return;
+            }
+            Console.Write("\nIle jaj składa ptak? ");
+            if (!int.TryParse(Console.ReadLine(), out int numberOfEgs)) return;
+            Egg[] eggs = new bird.LayEggs(numberOfEgs);
+            foreach egg in eggs)
+            {
+                Console.Write(eggs.Description);
+            }
+
+        }
+    }
+}
