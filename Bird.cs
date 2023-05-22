9a8ef40924cba6aff40d2b944676706b1dcde6c2
Kamil <mentos.96@o2.pl>
2023-05-22 20:25:16 +0200

    Write class Bird

---
+++ Bird.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIrd_app_for_traning_with_overriding_classes
+{
+    internal class Bird
+    {
+        public static Random Randomizer = new Random();
+        public virtual Egg[] LayEggs(int numberOfEggs)
+        {
+            Console.Error.WriteLine("Metoda Bird.LayEggs nie powinna być wywoływana");4
+            return new Egg[0];
+        }
+
+    }
+}
